f96a231a14198a94fe2f68a68d9b51bc5ea555f1
yfuka86 <yfuka86@gmail.com>
2016-02-16 03:20:18 +0900

    fix

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ import io from 'socket.io'
 const app = express()
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
-    res.header("Access-Control-Allow-Headers", "Content-Type")
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
-    next();
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+  next();
 })
 
 app.get('/distributor', (req, res) => {
@@ -23,7 +23,7 @@ app.get('/listener', (req, res) => {
 
 app.use('/static', express.static('static'));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 const server = http.Server(app).listen(port, () => {
   console.log('listening')
